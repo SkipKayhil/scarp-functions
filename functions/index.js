affb1a9eb03e20b86a41302a68c1f379da4a8032
SkipKayhil <skipkayhil@gmail.com>
2019-04-22 03:35:03 -0400

    employeeCreate and adminCreate rules

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.addAdminClaim = functions.firestore
+  .document('/admins/{uid}')
+  .onCreate((snap, context) => {
+    return admin
+      .auth()
+      .setCustomUserClaims(context.params.uid, { userType: 'admin' });
+  });
+
+exports.addEmployeeClaim = functions.firestore
+  .document('/employees/{uid}')
+  .onCreate((snap, context) => {
+    return admin
+      .auth()
+      .setCustomUserClaims(context.params.uid, { userType: 'employee' });
+  });
